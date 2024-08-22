30e3578f29d7196f0fb4993f9c865c39e47d1f58
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-08-22 11:49:56 +0200

    Initial commit

---
+++ custom.el
@@ -0,0 +1,35 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(acm-enable-icon nil)
+ '(column-number-mode t)
+ '(completion-category-overrides '((file (styles basic partial-completion))))
+ '(completion-styles '(orderless basic))
+ '(custom-enabled-themes '(modus-operandi-tinted))
+ '(delete-selection-mode t)
+ '(indent-tabs-mode nil)
+ '(lsp-bridge-enable-hover-diagnostic t)
+ '(modus-themes-bold-constructs t)
+ '(native-comp-async-report-warnings-errors 'silent)
+ '(native-comp-jit-compilation-deny-list '("lsp-bridge"))
+ '(package-selected-packages
+   '(consult embark embark-consult exec-path-from-shell expand-region
+             go-mode helpful keycast magit marginalia markdown-mode
+             multiple-cursors orderless vertico vterm wgrep yasnippet))
+ '(project-vc-ignores '("node_modules/"))
+ '(read-extended-command-predicate 'command-completion-default-include-p)
+ '(recentf-mode t)
+ '(save-place-mode t)
+ '(savehist-mode t)
+ '(scroll-bar-mode nil)
+ '(tab-always-indent 'complete)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "PragmataPro" :foundry "nil" :slant normal :weight regular :height 190 :width normal)))))
---
+++ init.el
@@ -0,0 +1,133 @@
+;;; -*- lexical-binding: t -*-
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;; Make C-m discernible from RET
+(define-key input-decode-map [?\C-m] [C-m])
+
+(bind-keys
+ ("C-c o" . customize-option)
+ ("C-<return>" . other-window)
+ ("C-;" . comment-dwim)
+ ("C-'" . jump-to-register)
+ ("M-'" . point-to-register))
+
+(defun my/toggle-display-line-numbers ()
+  "Toggle display of relative line numbers in the buffer."
+  (interactive)
+  (setq display-line-numbers
+        (unless display-line-numbers 'relative)))
+
+(bind-keys :prefix-map toggle-map
+           :prefix "C-c x"
+           ("n" . my/toggle-display-line-numbers))
+
+(use-package consult
+  :ensure t
+  :bind (("C-x b" . consult-buffer)
+         ("C-x 4 b" . consult-buffer-other-window)
+         ("C-x p b" . consult-project-buffer)
+         ("M-s g" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("C-M-'" . consult-register)))
+
+(use-package embark
+  :ensure t
+  :bind (("C-." . embark-act)
+         ("C-h b" . embark-bindings)))
+
+(use-package embark-consult
+  :ensure t)
+
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
+
+(use-package expand-region
+  :ensure t
+  :bind ("C-," . er/expand-region))
+
+(use-package go-mode
+  :ensure t
+  :mode "\\.go\\'")
+
+(use-package helpful
+  :ensure t
+  :bind (("C-h f" . helpful-callable)
+         ("C-h v" . helpful-variable)
+         ("C-h k" . helpful-key)
+         ("C-h x" . helpfu-command)
+         ("C-c C-d" . helpful-at-point)))
+
+(use-package keycast
+  :ensure t
+  :bind (:map toggle-map
+              ("k" . keycast-mode-line-mode)
+              ("h" . keycast-header-line-mode)))
+
+(use-package lsp-bridge
+  :demand t
+  :load-path "site-lisp/lsp-bridge"
+  :bind* (:map lsp-bridge-mode-map
+               ("M-." . lsp-bridge-find-def)
+               ("M-," . lsp-bridge-find-def-return)
+               ("M-?" . lsp-bridge-find-references)
+               ("M-n" . lsp-bridge-diagnostic-jump-next)
+               ("M-p" . lsp-bridge-diagnostic-jump-prev))
+  :config
+  (global-lsp-bridge-mode))
+
+(use-package magit
+  :ensure t)
+
+(use-package marginalia
+  :ensure t
+  :config
+  (marginalia-mode))
+
+(use-package markdown-mode
+  :ensure t
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode)))
+
+(use-package multiple-cursors
+  :ensure t
+  :bind (("<C-m> e" . mc/edit-lines)
+         ("<C-m> ^" . mc/edit-beginnings-of-lines)
+         ("<C-m> $" . mc/edit-ends-of-lines)
+         ("<C-m> x" . mc/mark-more-like-this-extended)
+         ("<C-m> a" . mc/mark-all-like-this-dwim)))
+
+(use-package orderless
+  :ensure t)
+
+(use-package vertico
+  :ensure t
+  :config
+  (vertico-mode))
+
+(use-package vterm
+  :ensure t
+  :bind (("C-c t" . vterm)
+         ("C-c 4 t" . vterm-other-window)))
+
+(use-package wgrep
+  :ensure t)
+
+(use-package which-key
+  :config
+  (which-key-mode))
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode))
