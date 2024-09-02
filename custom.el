3f29dfd810777aa9fcf98f1a8097c00f3aead809
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-09-02 20:01:50 +0200

    Increase avy timeout

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(acm-enable-icon nil)
- '(avy-timeout-seconds 0.2)
+ '(avy-timeout-seconds 0.3)
  '(column-number-mode t)
  '(completion-category-overrides '((file (styles basic partial-completion))))
  '(completion-styles '(orderless basic))
