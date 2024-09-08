08640a5d7afe3d5445867096c7fceb2ba7a8a86e
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-09-08 19:25:26 +0200

    Move all customization into init.el

--- init.el
+++ init.el
@@ -1,12 +1,24 @@
 ;;; -*- lexical-binding: t -*-
 
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(when (file-exists-p custom-file)
-  (load custom-file))
+;;; Theme and UI customization
+
+(custom-set-variables
+ '(column-number-mode t)
+ '(modus-themes-bold-constructs t)
+ '(modus-themes-to-toggle '(modus-operandi-tinted modus-vivendi-tinted))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+
+(custom-set-faces
+ '(default ((t (:family "PragmataPro Liga" :height 190)))))
 
 (require-theme 'modus-themes)
 
@@ -26,6 +38,20 @@
 (add-hook 'modus-themes-post-load-hook #'my/modus-themes-custom-faces)
 
 (modus-themes-load-theme 'modus-operandi-tinted)
+
+;;; Misc
+
+(custom-set-variables
+ '(delete-selection-mode t)
+ '(electric-pair-mode t)
+ '(indent-tabs-mode t)
+ '(native-comp-async-report-warnings-errors 'silent)
+ '(project-vc-ignores '("node_modules/"))
+ '(read-extended-command-predicate 'command-completion-default-include-p)
+ '(recentf-mode t)
+ '(save-place-mode t)
+ '(savehist-mode t)
+ '(tab-always-indent 'complete))
 
 (defun my/newline-above-and-indent ()
   "Insert a newline above the current line and move point to the beginning
@@ -50,6 +76,12 @@ of the line."
         (emacs-lisp-docstring-fill-column t))
     (fill-paragraph nil region)))
 
+(defun my/toggle-display-line-numbers ()
+  "Toggle display of relative line numbers in the buffer."
+  (interactive)
+  (setq display-line-numbers
+        (unless display-line-numbers 'relative)))
+
 ;; Make C-m discernible from RET
 (define-key input-decode-map [?\C-m] [C-m])
 
@@ -64,20 +96,18 @@ of the line."
  ("M-Q" . my/unfill-paragraph)
  ("<f5>" . modus-themes-toggle))
 
-(defun my/toggle-display-line-numbers ()
-  "Toggle display of relative line numbers in the buffer."
-  (interactive)
-  (setq display-line-numbers
-        (unless display-line-numbers 'relative)))
-
-(bind-keys :prefix-map toggle-map
+(bind-keys :prefix-map my-toggle-map
            :prefix "C-c x"
            ("n" . my/toggle-display-line-numbers)
            ("w" . whitespace-mode))
 
+;;; Packages
+
 (use-package avy
   :ensure t
   :bind ("C-;" . avy-goto-char-2)
+  :custom
+  (avy-timeout-seconds 0.3)
   :config
   (avy-setup-default))
 
@@ -134,12 +164,12 @@ of the line."
   :ensure t
   :bind (("M-$" . jinx-correct)
          ("C-M-$" . jinx-languages)
-         :map toggle-map
+         :map my-toggle-map
          ("j" . jinx-mode)))
 
 (use-package keycast
   :ensure t
-  :bind (:map toggle-map
+  :bind (:map my-toggle-map
          ("k" . keycast-mode-line-mode)
          ("h" . keycast-header-line-mode)))
 
@@ -154,7 +184,12 @@ of the line."
           ("C-. h" . lsp-bridge-popup-documentation)
           ("M-n" . lsp-bridge-diagnostic-jump-next)
           ("M-p" . lsp-bridge-diagnostic-jump-prev))
+  :custom
+  (acm-enable-icon nil)
+  (lsp-bridge-enable-hover-diagnostic t)
   :config
+  (add-to-list 'native-comp-jit-compilation-deny-list
+               '("lsp-bridge"))
   ;; Free the RET key for less intrusive behavior
   (keymap-unset acm-mode-map "RET")
   (global-lsp-bridge-mode))
@@ -181,7 +216,10 @@ of the line."
          ("<C-m> a" . mc/mark-all-like-this-dwim)))
 
 (use-package orderless
-  :ensure t)
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package org-mode
   :bind (("C-c c" . org-capture)
@@ -190,7 +228,11 @@ of the line."
          :map org-mode-map
          ("C-,") ; I use this for `er/expand-region'
          ("C-'")) ; I use this for `jump-to-register'
-  :hook (org-mode . turn-on-auto-fill))
+  :hook (org-mode . turn-on-auto-fill)
+  :custom
+  (org-agenda-files '("~/.todo.org"))
+  (org-default-notes-file "~/.todo.org")
+  (org-startup-indented t))
 
 (use-package pragmatapro-lig
   :diminish
@@ -199,7 +241,7 @@ of the line."
 
 (use-package rainbow-mode
   :ensure t
-  :bind (:map toggle-map
+  :bind (:map my-toggle-map
          ("r" . rainbow-mode)))
 
 (use-package treesit-auto
@@ -271,4 +313,6 @@ of the line."
          ("C-c j d" . jtsx-delete-jsx-node)
          ("C-c j t" . jtsx-toggle-jsx-attributes-orientation)
          ("C-c j h" . jtsx-rearrange-jsx-attributes-horizontally)
-         ("C-c j v" . jtsx-rearrange-jsx-attributes-vertically)))
+         ("C-c j v" . jtsx-rearrange-jsx-attributes-vertically))
+  :custom
+  (jtsx-enable-jsx-element-tags-auto-sync t))
