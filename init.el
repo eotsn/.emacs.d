179d84b983c1d5f10eb22a408131186c4bc38578
Eric Ottosson <4204520+eotsn@users.noreply.github.com>
2024-09-13 22:52:30 +0200

    Major refactoring

--- init.el
+++ init.el
@@ -6,23 +6,40 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 
-;;; Theme and UI customization
+(add-to-list 'load-path
+             (expand-file-name "site-lisp" user-emacs-directory))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; GUI
+;;
 
 (custom-set-variables
+ '(inhibit-startup-screen t)
  '(column-number-mode t)
- '(modus-themes-bold-constructs t)
- '(modus-themes-to-toggle '(modus-operandi-tinted modus-vivendi-tinted))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 
 (custom-set-faces
  '(default ((t (:family "PragmataPro Liga" :height 190)))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Theme customization
+;;
+
 (require-theme 'modus-themes)
+
+(custom-set-variables
+ '(modus-themes-bold-constructs t)
+ '(modus-themes-to-toggle '(modus-operandi-tinted modus-vivendi-tinted)))
+
+(bind-key "<f5>" #'modus-themes-toggle)
 
 (defun my/modus-themes-custom-faces (&rest _)
   (modus-themes-with-colors
     (custom-set-faces
+     ;; lsp-bridge
      `(acm-frame-border-face ((,c :background ,bg-active)))
      `(acm-frame-default-face ((,c :background ,bg-dim)))
      `(acm-frame-select-face ((,c :inherit bold :background ,bg-completion)))
@@ -33,101 +50,142 @@
      `(lsp-bridge-diagnostics-warning-face ((,c :underline (:style wave :color ,underline-warning))))
      `(lsp-bridge-kill-process ((,c :inherit bold :foreground ,err))))))
 
+;; Ensure that faces are updated when switching between themes
 (add-hook 'modus-themes-post-load-hook #'my/modus-themes-custom-faces)
 
 (modus-themes-load-theme 'modus-operandi-tinted)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;;; Misc
+;;
 
 (custom-set-variables
  '(delete-selection-mode t)
+ '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(electric-pair-mode t)
- '(indent-tabs-mode t)
+ '(indent-tabs-mode nil)
  '(native-comp-async-report-warnings-errors 'silent)
  '(project-vc-ignores '("node_modules/"))
  '(read-extended-command-predicate 'command-completion-default-include-p)
  '(recentf-mode t)
  '(save-place-mode t)
  '(savehist-mode t)
- '(tab-always-indent 'complete)
- '(ediff-window-setup-function 'ediff-setup-windows-plain))
+ '(tab-always-indent 'complete))
 
-(defun my/newline-above-and-indent ()
-  "Insert a newline above the current line and move point to the beginning
-of the line."
-  (interactive)
-  (previous-line)
-  (end-of-line)
-  (newline-and-indent))
-
-(defun my/newline-below-and-indent ()
-  "Insert a newline below the current line and move point to the beginning
-of the line."
-  (interactive)
-  (end-of-line)
-  (newline-and-indent))
-
-(defun my/unfill-paragraph (&optional region)
-  "Takes a multi-line paragraph and makes it into a single line of text."
-  (interactive (progn (barf-if-buffer-read-only) '(t)))
-  (let ((fill-column (point-max))
-        ;; This would override `fill-column' if it's an integer.
-        (emacs-lisp-docstring-fill-column t))
-    (fill-paragraph nil region)))
-
-(defun my/toggle-display-line-numbers ()
-  "Toggle display of relative line numbers in the buffer."
-  (interactive)
-  (setq display-line-numbers
-        (unless display-line-numbers 'relative)))
+;; Enable some useful commands which are disabled by default
+(dolist (cmd '(downcase-region
+               narrow-to-region
+               upcase-region))
+  (put cmd 'disabled nil))
 
 ;; Make C-m discernible from RET
 (define-key input-decode-map [?\C-m] [C-m])
 
-(bind-keys
- ("C-c o" . customize-option)
- ("C-<return>" . other-window)
- ("C-c ;" . comment-dwim)
- ("C-x C-d" . duplicate-line)
- ("C-'" . jump-to-register)
- ("M-'" . point-to-register)
- ("M-o" . my/newline-below-and-indent)
- ("M-O" . my/newline-above-and-indent)
- ("M-Q" . my/unfill-paragraph)
- ("<f5>" . modus-themes-toggle))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Personal utilities and bindings
+;;
+
+(defun my/unfill-paragraph (&optional region)
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
+(defun my/toggle-display-line-numbers ()
+  (interactive)
+  (setq display-line-numbers
+        (unless display-line-numbers 'relative)))
+
+(bind-keys ("C-;" . comment-dwim)
+           ("C-c M-q" . my/unfill-paragraph)
+           ("C-x C-d" . duplicate-line)
+           ("C-x C-e" . pp-eval-last-sexp)
+           ("C-<return>" . other-window))
 
 (bind-keys :prefix-map my-toggle-map
            :prefix "C-c x"
+           ("h" . hl-line-mode)
            ("n" . my/toggle-display-line-numbers)
-           ("w" . whitespace-mode))
+           ("w" . whitespacle-mode))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
 ;;; Packages
+;;
 
 (use-package avy
   :ensure t
-  :bind ("C-;" . avy-goto-char-2)
+  :bind ("C-'" . avy-goto-char-2)
   :custom
-  (avy-timeout-seconds 0.3)
+  (avy-single-candidate-jump nil)
   :config
   (avy-setup-default))
 
 (use-package consult
   :ensure t
-  :bind (("C-x b" . consult-buffer)
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)
+         ("C-x b" . consult-buffer)
          ("C-x 4 b" . consult-buffer-other-window)
+         ("C-x 5 b" . consult-buffer-other-frame)
+         ("C-x t b" . consult-buffer-other-tab)
+         ("C-x r b" . consult-bookmark)
          ("C-x p b" . consult-project-buffer)
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)
+         ("M-g g" . consult-goto-line)
+         ("M-g M-g" . consult-goto-line)
+         ("M-g o" . consult-outline)
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s f" . consult-fd)
          ("M-s g" . consult-ripgrep)
          ("M-s l" . consult-line)
-         ("C-M-'" . consult-register)))
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)
+         ("M-s e" . consult-isearch-history)
+         ("M-s l" . consult-line) ; Needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi) ; Needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)
+         ("M-r" . consult-history))
+  :custom
+  (consult-narrow-key "<")
+  (register-preview-function #'consult-register-format)
+  (xref-show-definitions-function #'consult-xref)
+  (xref-show-xrefs-function #'consult-xref)
+  :config
+  (advice-add #'register-preview :override #'consult-register-window))
 
 (use-package diminish :ensure t :defer t)
 
-(use-package eldoc :diminish)
-
 (use-package embark
   :ensure t
-  :bind (("M-." . embark-act)
-         ("C-h b" . embark-bindings)))
+  :bind (("C-." . embark-act)
+         ("C-h b" . embark-bindings))
+  :config
+  (setq prefix-help-command #'embark-prefix-help-command))
 
 (use-package embark-consult
   :ensure t)
@@ -140,13 +198,7 @@ of the line."
 
 (use-package expand-region
   :ensure t
-  :bind (("C-," . er/expand-region)
-         :prefix-map
-         my-expand-region-map
-         :prefix "C-c ,"
-         ("q" . er/mark-inside-quotes)
-         ("b" . er/mark-inside-pairs)
-         ("m" . er/mark-method-call)))
+  :bind ("C-=" . er/expand-region))
 
 (use-package forge
   :ensure t
@@ -154,30 +206,29 @@ of the line."
 
 (use-package format-all
   :commands format-all-mode
-  :hook ((prog-mode . format-all-mode)
-	 (format-all-mode . format-all-ensure-formatter))
+  :hook (prog-mode . format-all-mode)
   :config
+  (add-hook 'format-all-mode-hook #'format-all-ensure-formatter)
   (setq-default format-all-formatters
 		'(("Lua" (stylua "-s")))))
 
 (use-package git-link
   :ensure t
-  :bind ("C-c g l" . git-link))
+  :bind ("C-c Y" . git-link))
 
 (use-package helpful
   :ensure t
-  :bind (("C-h f" . helpful-callable)
-         ("C-h v" . helpful-variable)
-         ("C-h k" . helpful-key)
-         ("C-h x" . helpfu-command)
-         ("C-c C-d" . helpful-at-point)))
+  :bind (("C-c C-d" . helpful-at-point)
+         ([remap describe-function] . helpful-callable)
+         ([remap describe-key] . helpful-key)
+         ([remap describe-variable] . helpful-variable)
+         ([remap describe-command] . helpful-command)))
 
 (use-package jinx
   :ensure t
-  :bind (("M-$" . jinx-correct)
-         ("C-M-$" . jinx-languages)
-         :map my-toggle-map
-         ("j" . jinx-mode)))
+  :bind (("M-$" . jinx-correct) ; orig. `ispell-word'
+         ("C-M-$" . jinx-languages))
+  :hook ((org-mode markdown-mode) . jinx-mode))
 
 (use-package keycast
   :ensure t
@@ -188,14 +239,19 @@ of the line."
 (use-package lsp-bridge
   :demand t
   :load-path "site-lisp/lsp-bridge"
-  :bind* ( :map lsp-bridge-mode-map
-           ("C-. d" . lsp-bridge-find-def)
-           ("C-. r" . lsp-bridge-find-references)
-           ("C-. D" . lsp-bridge-peek)
-           ("C-. e" . lsp-bridge-diagnostic-list)
-           ("C-. h" . lsp-bridge-popup-documentation)
-           ("M-n" . lsp-bridge-diagnostic-jump-next)
-           ("M-p" . lsp-bridge-diagnostic-jump-prev))
+  :bind ( :map lsp-bridge-mode-map
+          ("M-n" . lsp-bridge-diagnostic-jump-next)
+          ("M-p" . lsp-bridge-diagnostic-jump-prev)
+          ("s-l G d" . lsp-bridge-peek)
+          ("s-l a" . lsp-bridge-code-action)
+          ("s-l e" . lsp-bridge-diagnostic-list)
+          ("s-l g d" . lsp-bridge-find-def)
+          ("s-l g h" . lsp-bridge-incoming-call-hierarchy)
+          ("s-l g i" . lsp-bridge-find-impl)
+          ("s-l g r" . lsp-bridge-find-references)
+          ("s-l g t" . lsp-bridge-find-type-def)
+          ("s-l h" . lsp-bridge-popup-documentation)
+          ("s-l r" . lsp-bridge-rename))
   :custom
   (acm-enable-icon nil)
   (lsp-bridge-enable-hover-diagnostic t)
@@ -223,11 +279,16 @@ of the line."
 
 (use-package multiple-cursors
   :ensure t
-  :bind (("<C-m> e" . mc/edit-lines)
+  :bind (("<C-m> $" . mc/edit-ends-of-lines)
          ("<C-m> ^" . mc/edit-beginnings-of-lines)
-         ("<C-m> $" . mc/edit-ends-of-lines)
+         ("<C-m> W" . mc/mark-all-words-like-this)
+         ("<C-m> Y" . mc/mark-all-symbols-like-this)
+         ("<C-m> a" . mc/mark-all-like-this-dwim)
+         ("<C-m> n" . mc/mark-next-like-this)
+         ("<C-m> p" . mc/mark-previous-like-this)
+         ("<C-m> w" . mc/mark-next-like-this-word)
          ("<C-m> x" . mc/mark-more-like-this-extended)
-         ("<C-m> a" . mc/mark-all-like-this-dwim)))
+         ("<C-m> y" . mc/mark-next-like-this-symbol)))
 
 (use-package orderless
   :ensure t
@@ -236,12 +297,9 @@ of the line."
   (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package org-mode
-  :bind (("C-c c" . org-capture)
-         ("C-c a" . org-agenda)
-         ("C-c s" . org-store-link)
-         :map org-mode-map
-         ("C-,") ; I use this for `er/expand-region'
-         ("C-'")) ; I use this for `jump-to-register'
+  :bind (("C-c a" . org-agenda)
+         ("C-c c" . org-capture)
+         ("C-c s" . org-store-link))
   :hook (org-mode . turn-on-auto-fill)
   :custom
   (org-agenda-files '("~/.todo.org"))
@@ -250,7 +308,6 @@ of the line."
 
 (use-package pragmatapro-lig
   :diminish
-  :load-path "site-lisp"
   :hook (prog-mode text-mode))
 
 (use-package rainbow-mode
@@ -269,64 +326,28 @@ of the line."
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
 
-(use-package vertico
-  :ensure t
-  :config
-  (vertico-mode))
-
 (use-package vterm
   :ensure t
   :bind (("C-c t" . vterm)
          ("C-c 4 t" . vterm-other-window)))
 
-(use-package vundo
+(use-package vertico
   :ensure t
-  :bind ("C-c C-/" . vundo)
   :config
-  (setq vundo-glyph-alist vundo-unicode-symbols))
+  (vertico-mode))
 
 (use-package wgrep
-  :ensure t)
-
-(use-package which-key
-  :diminish
-  :config
-  (which-key-mode))
+  :ensure t
+  :custom
+  (wgrep-auto-save-buffer t))
 
 (use-package yasnippet
   :ensure t
   :diminish yas-minor-mode
-  :bind ( :map yas-minor-mode-map
-          ("TAB") ; I use a dedicated key for snippet expansion
-          ("C-<tab>" . yas-expand))
-  :config
-  (yas-global-mode))
-
-;; This package must be loaded after `treesit-auto' to override the
-;; entries inserted into `auto-mode-alist'. Because `:after' doesn't
-;; seem to work we have to rely on ordering instead.
-(use-package jtsx
-  :ensure t
-  :mode (("\\.tsx\\'" . jtsx-tsx-mode)
-         ("\\.ts\\'" . jtsx-typescript-mode))
-  :hook ((jtsx-tsx-mode . hs-minor-mode)
-         (jtsx-typescript-mode . hs-minor-mode))
-  :bind ( :map jtsx-tsx-mode-map
-          ("C-c C-j" . jtsx-jump-jsx-element-tag-dwim)
-          ("C-c j o" . jtsx-jump-jsx-opening-tag)
-          ("C-c j c" . jtsx-jump-jsx-closing-tag)
-          ("C-c j r" . jtsx-rename-jsx-element)
-          ("C-c <down>" . jtsx-move-jsx-element-tag-forward)
-          ("C-c <up>" . jtsx-move-jsx-element-tag-backward)
-          ("C-c C-<down>" . jtsx-move-jsx-element-forward)
-          ("C-c C-<up>" . jtsx-move-jsx-element-backward)
-          ("C-c C-S-<down>" . jtsx-move-jsx-element-step-in-forward)
-          ("C-c C-S-<up>" . jtsx-move-jsx-element-step-in-backward)
-          ("C-c j w" . jtsx-wrap-in-jsx-element)
-          ("C-c j u" . jtsx-unwrap-jsx)
-          ("C-c j d" . jtsx-delete-jsx-node)
-          ("C-c j t" . jtsx-toggle-jsx-attributes-orientation)
-          ("C-c j h" . jtsx-rearrange-jsx-attributes-horizontally)
-          ("C-c j v" . jtsx-rearrange-jsx-attributes-vertically))
-  :custom
-  (jtsx-enable-jsx-element-tags-auto-sync t))
+  :bind (("C-c y f" . yas-visit-snippet-file)
+         ("C-c y i" . yas-insert-snippet)
+         ("C-c y n" . yas-new-snippet)
+         ("C-c y r" . yas-reload-all)
+         :map yas-minor-mode-map
+         ("C-<tab>" . yas-expand))
+  :hook (prog-mode . yas-minor-mode))
